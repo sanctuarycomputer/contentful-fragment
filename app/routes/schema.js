02803bfc65720624b198256e4b89b2110f0d4537
Hugh Francis <hughfrancis89@gmail.com>
2018-08-30 22:15:36 -0400

    Fix

--- app/routes/schema.js
+++ app/routes/schema.js
@@ -2,7 +2,7 @@ import Route from '@ember/routing/route';
 
 export default Route.extend({
   model() {
-    return this.modelFor('application').field.getValue()._schema || [];
+    return ((this.modelFor('application').field.getValue() || {})._schema || []);
   },
   setupController(controller) {
     this._super(...arguments);
